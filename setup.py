5389f5a0d6726dbeb45c8ed1e7a2b279385c4e02
znnnnbb <zainab.hyder786@gmail.com>
2025-02-27 03:07:00 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'generative_ai_project',
+    version = '0.0.0',
+    author= 'Zainab',
+    author_email= 'zainab.hyder786@gmail.com',
+    packages= find_packages(),
+    install_requires =[]
+    
+)
